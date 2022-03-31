6c6ef2eeec80d6aae2f495d27e702c86ed2e2df8
Chris Field <94464835+crfield18@users.noreply.github.com>
2022-03-31 16:26:31 +0100

    clean up

--- j-names.py
+++ j-names.py
@@ -3,19 +3,18 @@ namesdb = open('../names-database/NameDatabases/NamesDatabases/first names/all.t
 jnamesdb = {'j-names' : []}
 jnames = []
 lines = 0
+
+# Open list of first names from names-database
 for line in namesdb:
     lines += 1
+    # Add all J names to list
     if line.startswith('J'):
         jnames.append(line.strip('\n'))
     else:
         pass
+# Add J names list to jnames database    
 jnamesdb['j-names'] += jnames
 
-
-
+# Dump J names to j-names.json
 with open("j-names.json", 'w') as db:
     json.dump(jnamesdb, db)
-
-
-db1 = json.load(open('j-names.json', 'r'))["j-names"]
-print(db1[7])
