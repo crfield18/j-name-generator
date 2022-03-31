2c7ba00cfa38e22c1de0feed451dcddcc24fd541
Chris Field <94464835+crfield18@users.noreply.github.com>
2022-03-31 17:43:06 +0100

    basics

--- js/generator.js
+++ js/generator.js
@@ -1,13 +1,12 @@
 // Import J names from j-names.json
+const jnamesdb = require('./j-names.json')["j-names"];
+let db_length = parseInt(jnamesdb.length);
 
+// console.log(jnamesdb["j-names"][7]);
 
-const jnamesdb = require('j-names.json');
-console.log(jnamesdb)
+function jname() {
+    let rand_int = Math.floor(Math.random() * db_length); // Generate random int between 0 and length of jnames.json
+    let rand_name = jnamesdb[rand_int]; // 
+    return(rand_int + ': ' + rand_name)
 
-function rand()
-{
-    let rand_int = Math.floor(Math.random() * 10);
-    let rand_name = jnamesdb[rand_int];
-    return this.rand_name
 }
-    
